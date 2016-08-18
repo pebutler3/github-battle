ca1b4676194b38531305efbbc635e9d1cb7d096c
pebutler3 <p.e.butler3@gmail.com>
2016-08-18 08:17:47 -0600

    created loading component to utilize defaultProps

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
 var MainContainer = require('../components/MainContainer');
+var Loading = require('./Loading');
 
 // function puke(obj) {
 // 	return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
@@ -24,7 +25,7 @@ function Results (props) {
 
 	if(props.isLoading == true) {
 		return (
-			<p> LOADING </p>
+			<Loading />	
 		)
 	}
 
